6d5f71f733203abdda67c13c2e6efb8056cc40ab
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-08 17:29:17 +0200

    Added mocks

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -2,10 +2,19 @@ package edu.iis.mto.testreactor.coffee;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.coffee.milkprovider.MilkProvider;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 class CoffeeMachineTest {
+
+    @Mock
+    private Grinder mockGrinder;
+    @Mock
+    private MilkProvider milkProviderMock;
+    @Mock
+    private CoffeeReceipes coffeeRecipesMock;
 
     @Test
     public void itCompiles() {
