f7cf0cedc637ccb5489403c6dc25032187801449
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-08 17:53:51 +0200

    Added next implementation of behavioral test case, this time test checks interaction with CoffeeReceipes interface.

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -2,14 +2,19 @@ package edu.iis.mto.testreactor.coffee;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 import edu.iis.mto.testreactor.coffee.milkprovider.MilkProvider;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class CoffeeMachineTest {
 
     @Mock
@@ -25,7 +30,7 @@ class CoffeeMachineTest {
     @BeforeEach
     void setUp() {
         defaultTestReceipe = CoffeeReceipe.builder().build();
-        defaultOrder = CoffeOrder.builder().build();
+        defaultOrder = CoffeOrder.builder().withSize(CoffeeSize.STANDARD).withType(CoffeType.CAPUCCINO).build();
     }
 
     @Test
@@ -44,12 +49,20 @@ class CoffeeMachineTest {
         Executable invalidConstructorCall = () -> new CoffeeMachine(mockGrinder, null, coffeeRecipesMock);
         assertThrows(NullPointerException.class, invalidConstructorCall);
     }
+    
     @Test
     void passingNullCoffeeRecipesShouldCauseNPEThrownDuringCoffeeMachineInitialization() {
         Executable invalidConstructorCall = () -> new CoffeeMachine(mockGrinder, milkProviderMock, null);
         assertThrows(NullPointerException.class, invalidConstructorCall);
     }
 
-
-
+    @Test
+    void ifCoffeeReceipesGetReceipeMethodReturnedNullReceipeThenCoffeeMachineShouldThrowUnsupportedCoffeeException() {
+        when(coffeeRecipesMock.getReceipe(Mockito.any())).thenReturn(null);
+        when(mockGrinder.canGrindFor(Mockito.any())).thenReturn(true);
+        when(mockGrinder.grind(Mockito.any())).thenReturn(23.0);
+        var testedMachine = new CoffeeMachine(mockGrinder, milkProviderMock, coffeeRecipesMock);
+        Executable invalidMakeCoffeeCall = () -> testedMachine.make(defaultOrder);
+        assertThrows(UnsupportedCoffeeException.class, invalidMakeCoffeeCall);
+    }
 }
