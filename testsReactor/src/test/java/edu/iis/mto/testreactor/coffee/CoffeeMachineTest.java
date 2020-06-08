ddb543a8f2245cdca5d6bdf7237b4fe2b3e4ef67
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-06-08 17:34:23 +0200

    Added first behavior tests checking correctness of constructor behaviour.

--- testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/coffee/CoffeeMachineTest.java
@@ -1,10 +1,12 @@
 package edu.iis.mto.testreactor.coffee;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import edu.iis.mto.testreactor.coffee.milkprovider.MilkProvider;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 
 class CoffeeMachineTest {
@@ -19,6 +21,23 @@ class CoffeeMachineTest {
     @Test
     public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
+    }
+
+    @Test
+    void passingNullGrinderShouldCauseNPEThrownDuringCoffeeMachineInitialization() {
+        Executable invalidConstructorCall = () -> new CoffeeMachine(null, milkProviderMock, coffeeRecipesMock);
+        assertThrows(NullPointerException.class, invalidConstructorCall);
+    }
+
+    @Test
+    void passingNullMilkProviderShouldCauseNPEThrownDuringCoffeeMachineInitialization() {
+        Executable invalidConstructorCall = () -> new CoffeeMachine(mockGrinder, null, coffeeRecipesMock);
+        assertThrows(NullPointerException.class, invalidConstructorCall);
+    }
+    @Test
+    void passingNullCoffeeRecipesShouldCauseNPEThrownDuringCoffeeMachineInitialization() {
+        Executable invalidConstructorCall = () -> new CoffeeMachine(mockGrinder, milkProviderMock, null);
+        assertThrows(NullPointerException.class, invalidConstructorCall);
     }
 
 }
